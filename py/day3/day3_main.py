bfbc2139f34931248eb3c980c968c3e7bb7a4676
John Nicholas <john.nicholas@dinoroar.io>
2020-12-03 14:57:53 +0000

    day32 py

--- py/day3/day3_main.py
+++ py/day3/day3_main.py
@@ -4,8 +4,17 @@ from day3.slope import Slope
 def day31():
     data = open("data.txt").read()
     slope = Slope(data)
-    print(slope.get_number_of_trees(3,1))
+    print(slope.get_number_of_trees(3, 1))
+
+
+def day32():
+    data = open("data.txt").read()
+    slope = Slope(data)
+    res = slope.get_number_of_trees(1, 1) * slope.get_number_of_trees(3, 1) * slope.get_number_of_trees(5,
+                                                                                                        1) * slope.get_number_of_trees(
+        7, 1) * slope.get_number_of_trees(1, 2)
+    print(res)
 
 
 if __name__ == '__main__':
-    day31()
+    day32()
